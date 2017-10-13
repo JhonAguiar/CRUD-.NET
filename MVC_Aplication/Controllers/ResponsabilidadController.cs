9deeb74c5192182e4b1d9307367c7baec1d11abc
admin <jhon.aguiar93@gmail.com>
2017-10-13 16:30:17 -0500

    jhon

---
+++ MVC_Aplication/Controllers/ResponsabilidadController.cs
@@ -0,0 +1,91 @@
+﻿using MVC_Aplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Aplication.Controllers
+{
+    public class ResponsabilidadController : Controller
+    {
+        RVDESAEntities rv = new RVDESAEntities();
+        // GET: Responsabilidad
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Responsabilidad/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Responsabilidad/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Responsabilidad/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Responsabilidad/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Responsabilidad/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Responsabilidad/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Responsabilidad/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC_Aplication/Controllers/UsuarioController.cs
@@ -0,0 +1,111 @@
+﻿using MVC_Aplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Aplication.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        RVDESAEntities db = new RVDESAEntities();
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View(db.SYS_USUARIO.ToList<SYS_USUARIO>());
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(int id)
+        {
+            var query = (from A in db.SYS_USUARIO where A.PK_ID_SYS_USUARIO == id select A).Single();
+
+            return View((SYS_USUARIO)query);
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "PK_ID_SYS_USUARIO")] SYS_USUARIO ObjUsuario)
+        {
+            try
+            {
+                if (ModelState.IsValid) {
+                    return View();
+                }
+                db.Entry(ObjUsuario).State = EntityState.Added;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var query = (from A in db.SYS_USUARIO where A.PK_ID_SYS_USUARIO == id select A).Single();
+
+            return View((SYS_USUARIO)query);
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, SYS_USUARIO ObjUsuario)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (!ModelState.IsValid) {
+                    return View();
+                }
+                db.Entry(ObjUsuario).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var query = (from A in db.SYS_USUARIO where A.PK_ID_SYS_USUARIO == id select A).Single();
+
+            return View((SYS_USUARIO)query);
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, SYS_USUARIO objUsuario)
+        {
+            try
+            {
+                var query = (from A in db.SYS_USUARIO where A.PK_ID_SYS_USUARIO == id select A).Single();
+                // TODO: Add delete logic here
+                objUsuario = (SYS_USUARIO)query;
+                db.Entry(objUsuario).State = EntityState.Deleted;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
